2c0d5eab96bf6705e2e2522c7937053b26ec22c5
SEan <hi.na3s@gmail.com>
2013-05-02 01:58:17 -0700

    Initial demo services and directives

--- demo/directives.js
+++ demo/directives.js
@@ -2,7 +2,6 @@
 
 blink
 	.directive('pixelpad', function() {
-		
     'use strict';
 
     return {
@@ -11,30 +10,40 @@ blink
            value: '=ngModel',
        },
        link:function($scope,elm,$attrs,uiEvent ) {
+	
+				var $pad = $(elm);
+				var key = $attrs.key;			
+				$scope.pixels.push = [];
 				
-				var $this = $(elm);
+			}
+   }
 
-        var expression,
-        options = {
-          range: true,
-          values: scope.values,
-          slide: function(event,ui){
-            scope.$apply(function(){
-					 		scope.option.current = ui.value;
-          	})
-					}
-        };
-       
-			 if ($attrs.uiSlider) {
-         expression = scope.$eval($attrs.uiSlider);
-       } else {
-         expression = {};
-       }
+	.directive('acc', function(){
+		
+		return {
+			link: function($scope) {
+		 		$(window).bind('acc', function(e) {
+			  	$scope.acc = {};
+					$scope.acc.x = e.accX;
+					$scope.acc.y = e.accY;
+					$scope.acc.z = e.accZ ? e.accZ : null;
+				});
+			}
+		};
+		
+	});
 
-        //Set the options from the directive's configuration
-       angular.extend(options, uiConfig.devCalendar, expression);
-       // console.log(options);
-       elm.slider(options);
-       }
+	.directive('shake', function(){
+		
+		window.addEventListener('shake', shakeEventDidOccur, false);
 
-   };
+		//function to call when shake occurs
+		function shakeEventDidOccur () {
+
+		    //put your own code here etc.
+		    if (confirm("Undo?")) {
+						
+		    }
+		}
+		
+	});
--- demo/services.js
+++ demo/services.js
@@ -1,21 +1,6 @@
 'use strict';
 
 blink
-	.factory('Effects', function ($resource) {
-
-		return {
-			list : function($scope){
-				var ListEffectsJson = function(data) {
-				    $scope.assets = data;
-						for(var i=0;i<$scope.assets.length;i++){
-							for(var k=0;k<$scope.assets[i].options.length;k++){
-								$scope.assets[i].options[k].current = $scope.assets[i].options[k].default;
-							}
-						}
-				}
-				var url = "http://192.168.1.6:8888/effects";
-				$http.jsonp(url);
-			}
-		}
-	
-	});
+	.factory('AddFrame', function ($resource) {
+		return $resource( 'http://192.168.1.6/:id/', {id:'@id'} ,  )
+	})
