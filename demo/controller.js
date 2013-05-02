67d06461d9426a4c634e700448328323c37030fe
SEan <hi.na3s@gmail.com>
2013-05-02 01:57:57 -0700

    Controller

--- demo/controller.js
+++ demo/controller.js
@@ -1,161 +1,97 @@
-<!doctype html>
-<html lang="en" ng-app="blinken">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>LEDs</title>
-  <link rel="stylesheet" href="css/bootstrap.css">
-  <link rel="stylesheet" href="css/bootstrap-responsive.css">
-  <link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="/lib/angular-ui/angular-ui.min.css"/>
-	<link rel="stylesheet" href="/lib/jquery/plugins/spectrum/spectrum.css"/>
-	<link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/themes/redmond/jquery-ui.css" rel="stylesheet" />
-  
-	
-</head>
-<body ng-controller="LedCtrl" id="body" ng-mouseup="mouseUp($event)" style="background-color:rgb(50, 50, 50)">
-	
-	<!-- <div class="container" style="max-width: 600px" >
-    <ul>
-      <li><div class="btn btn-primary" ng-click="turnOff()">reset</div></li>
-      <li ng-repeat="effect in effects">
-        <div class="btn btn-primary" ng-click="registerEffect(effect)">{{effect.name}}</div>
-        <div><pre>{{effect.options}}</pre></div>
-      </li>
-    </ul>
-  </div> -->
+//It's 12:00am
 
-	<section id="leds" class="preview on" style="width:{{ledsWidth}}px;" documentwidth>
-		<!-- <div ng-mousemove="mouseMove($event)" >  -->
-      <ul>
-        <li class="led" ng-repeat="led in leds" style="width:{{ledRes}}px;height:{{ledRes}}px;position:relative;">
-          <div style="background-color:rgb({{led.rgb[0]}}, {{led.rgb[1]}}, {{led.rgb[2]}}); width:50%;margin:50%; ">
-            <!-- ng-mousedown="mouseDown(led, $event)" -->
-            <!-- te-touchstart="touchStart(led, $event)" -->
-            <!-- te-touchmove="touchMove($event)" -->
-            <!-- te-touchend="mouseUp($event)"> -->
-&nbsp;</div>
-        </li>
-      </ul>
-    <!-- </div> -->
-	</section>
-	
-<!-- 	<header>
-		<ul id="effect-selector">
-				<li ng-repeat="effect in effects">
-					<a class="btn btn-primary" href="#" id="{{effect.name}}" ng-click="registerEffect(effect)">{{effect.name}}</a>
-				</li>
-				<a class="btn btn-primary" href="#" id="{{effect.name}}" ng-click="turnOff()">Reset</a>
-			</ul>
-		</header> -->
-	
-	<section id="secondary">
+blink
+	.controller('PixelPad', function($timeout, AddFrame){
 		
-		<header search-filter>
-			<ul></ul>
-		</header>
+		$scope.active = false;
 		
-		<article id="library">
-			<ul>
-				<li selector id="{{effect.name}}" asset-type="{{effect.type}}" ng-click="applyToSelectedLayer()" ng-repeat="effect in effects"><a ng-click="registerEffect(effect)">{{effect.name}}</a></li>
-			</ul>
-		</article>
+		$scope.pixels = [ null, null, null ];
+		$scope.history = [];
 		
-	</section>
-	
-	<section id="primary" >
-		<!-- <tabs> -->
-			<!-- <pane title="{{effect.name}}"> -->
- 			<article class="effect block" ng-repeat="effect in effects" masonry>
-				<header>{{effect.name}}</header>
-				
-				<form id="{{effect.name}}" class="params">
-					<ul>
-						<li ng-repeat="option in effect.options" class="parameter">
-							<label>{{option.name}}</label>
-							<!-- <wijslider value="{{option.current}}" min="{{option.min && option.min || 0}}" max="{{option.max && option.max || 100}}"></wijslider> -->
+		// $scope.hsv = [];
+		$scope.xyz = [];
+		
+		$scope.timeout = 500;
+		
+		//Idle
+		$scope.idle = 0;
+		$scope.idleThreshold = 30;
+		
+		//This updates the server every so often.
+		var update = function() {
+		    cancelRefresh = $timeout(function update() {
+						//Activity based sessions.
+						$scope.session();
+						//Query accelerometer & generate HSCV
+						// $scope.interpretMotion();
+						//send the frame data
+						AddFrame.save($scope.pixels);
+						//Calculate some meta.
+						$scope.meta();
+						//Cache the data (history)
+						$scope.cache();
+						//Reset the frame;
+						$scoope.purge();
+						//Infinite loop
+		        cancelRefresh = $timeout(update, $scope.timeout);
+		    }, $scope.timeout);
+		};
+		
+		//Cancels timeout in case views disrupts us, fail safe
+		$scope.$on('$destroy', function(e) {
+		    $timeout.cancel(update);
+		});
+		
+		$scope.session = function(){
+			var isPixels = $scope.isPixels();
+			if(!isPixels) $scope.idle++; //Increase the idle count
+			if($scope.active == false && isPixels) $scope.active = true; //Nobody has been using it, but surprise, there's pixel data... active is now true.
+		}
+		
+		$scope.isPixels = function(){
+			for(var p=0;p<3;p++) {
+				if(typeof $scope.pixels[p] == 'array') return true;
+			}
+		}
+		
+		$scope.cache = function(){
+			var cache = {};
+					cache.color = $scope.pixels;
+					cache.virgin = $scope.isPixels();
 			
-						
-							<div ng-switch="option.type" class="option">
-							
-							    <div ng-switch-when="color">
-							    	<input spectrum="{ color: '#ffffff' }" />
-							    </div>
-						
-									<div ng-switch-when="select">
-										<select ng-model="option.default">
-											<option ng-repeat="o in option.options" ng-value="o">{{o}}</option>
-										</select>
-									</div>
-								
-									<div ng-switch-when="slider">
-										<div ui-slider="{ 'min' : 0, 'max' : 100}" ng-model="option.current"></div>
-									</div>
-								
-									<div ng-switch-when="stepper">
-									
-									</div>
-								
-									<div ng-switch-when="integer">
-										<div ui-slider="{ 'min' : 0, 'max' : 100}" ng-model="option.current"></div>
-									</div>
-							</div>
-						
-						</li>
-					</ul>
-					<!-- <div><pre>{{effect.options}}</pre></div> -->
-			</form>
-			<!-- </pane> -->
-		</article>
-	</section>
-	<!-- </tabs> -->
-	
-	
-	
-	<footer>
+			$scope.history.unshift(cache);
+		};
 		
-	</footer>
+		$scope.purge = function(){
+			$scope.pixels = [ null, null, null ];
+		}
+		
+		//If the number of idle frames is less than the idea_threshold than someone is using the controller.
+		$scope.resetSession = function(){
+			if($scope.idle < $scope.idleThreshold) return ($scope.active=true);
+			return ($scope.active=true)
+		};
+		
+		//method to turn activate and deactive an active pixel.
+		$scope.togglePixel = function(key){
+			var state = ($scope.pixels[key] && $scope.isPixels);
+				$scope.pixels[key] = (state) ? $scope.motionToHSV() : null;
+			}
+		};
+		
+		//Normalizes accelerometer values between 0 & 255
+		$scope.normalize = function(x){
+			return Math.round( 1 + (x-(-2.5)) * (255-1) / ((2.5)-(-2.5)) )
+		};
+		
+		//Take acceleratomenter data and transform into HSV
+		$scope.motionToHSV = function(){
+			var a = $scope.normalize($scope.acc.x);
+			var b = $scope.normalize($scope.acc.y);
+			var c = $scope.normalize($scope.acc.z);
+			return [a,b,c]
+		};
+		
+	})
 	
-
-	
-	<!-- jQuery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js" type="text/javascript"></script>
-	
-
-	<!-- Wijmo -->
-	<!-- <script src="http://cdn.wijmo.com/jquery.wijmo-open.all.2.3.1.min.js" type="text/javascript"></script>
-	<script src="http://cdn.wijmo.com/jquery.wijmo-complete.all.2.3.1.min.js" type="text/javascript"></script> -->
-	
-	<script src="/lib/jquery/plugins/jquery.masonry.min.js"></script>
-	
-	<!-- Angular -->
-  <script src="/lib/angular.min.js"></script>
-  <script src="/lib/angular-resource.min.js"></script>
-  <script src="/lib/angular-touchevents.js"></script>
-
-	<!-- Angular -->
-  <script src="/lib/angular-ui/angular-ui.min.js"></script>
-
-	<!-- Color Picker -->
-  <script src="/lib/jquery/plugins/spectrum/spectrum.js"></script>
-	
-	<!-- Wijmo -->
-	<!--script src="http://cdn.wijmo.com/external/angular.wijmo.js"></script-->
-	
-	<script type="text/javascript">
-	blink = angular.module('blinken', ['teTouchevents','ngResource', 'ui']);
-	</script>
-	
-	<script src="js/utils.js"></script>
-	
-	<!-- Components -->
-  <script src="js/controllers.js"></script>
-  <script src="js/directives.js"></script>
-  <script src="js/services.js"></script>
-
-  <!-- For local testing, include host http://192.168.1.6:8888 -->
-  <script src="/socket.io/socket.io.js"></script>
-
-</body>
-</html>
+//It's 12:24 am.
