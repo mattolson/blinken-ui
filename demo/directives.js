112b92ca4eba8c93759726ff4653ae36b28a5013
SEan <hi.na3s@gmail.com>
2013-05-03 01:47:39 -0700

    Erp.

--- demo/directives.js
+++ demo/directives.js
@@ -14,9 +14,9 @@ demo
 		 		$(window).bind('acc', function(e) {
 					//console.log(e);
 			
-					if(e.accX) $scope.acc.x = (typeof e.accX === "float")  ? Math.round(e.accX*10)/10 : $scope.acc.x;
-					if(e.accY) $scope.acc.y = (typeof e.accY === "float")  ? Math.round(e.accY*10)/10 : $scope.acc.y;
-					if(e.accZ) $scope.acc.z = (typeof e.accZ === "float")  ? Math.round(e.accZ*10)/10 : $scope.acc.z;
+					if(e.accX) $scope.acc.x = (typeof e.accX === "float")  ? e.accX : $scope.acc.x;
+					if(e.accY) $scope.acc.y = (typeof e.accY === "float")  ? e.accY : $scope.acc.y;
+					if(e.accZ) $scope.acc.z = (typeof e.accZ === "float")  ? e.accZ : $scope.acc.z;
 				});
 			}
 		};
