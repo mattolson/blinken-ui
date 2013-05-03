5dd1b7ee335e834fc4a291d1026a46f3742bc9ae
SEan <hi.na3s@gmail.com>
2013-05-03 00:16:41 -0700

    Fixed Frame $resource

--- demo/services.js
+++ demo/services.js
@@ -5,15 +5,17 @@ demo
 
 	.factory('Frame', function ($resource) {
 		return $resource( 
-			'http://192.168.1.6/Layers/1/', 
-			{update: {method:'PUT'} } 
+			'http://192.168.1.6/Layers/:id', 
+			{ id: 1 },
+			{ update: { method:'PUT' } } 
 		);
 	})
 	
 	.factory('Shake', function($resource){
 		return $resource( 'http://192.163.1.6/Layers/2/', 
-		{source: 'sparkle'}, 
-		{ update : { method : 'PUT'} } )
+			{ source: 'sparkle' }, 
+			{ update : { method : 'PUT'} } 
+		)
 	})
 	
 	.factory('Members', function($resource){
