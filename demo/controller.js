4069c0888b1800b224a2fa84a2fb2067ab11d606
SEan <hi.na3s@gmail.com>
2013-05-04 13:59:31 -0700

    Updated layer ID

--- demo/controller.js
+++ demo/controller.js
@@ -292,7 +292,7 @@ function PixelPad($scope, $timeout, $http, Frame, Layers){
 		$scope.addFrame = function(){
 			
 			$scope.response = $http.put(
-				'http://192.168.1.6:8888/layers/3/',
+				'http://192.168.1.6:8888/layers/74/',
 				$scope.request, 
 				function(obj){
 					console.log(obj);
